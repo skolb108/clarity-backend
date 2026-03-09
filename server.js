5c8a1356c38013bcc9a696ecfbfbaaac73aa7ee4
Stephan Kolb <skolb@macbookpro.localdomain>
2026-03-09 14:30:25 +0100

    clean deploy

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-import http from "http";
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Clarity backend running");
+});
 
 const PORT = process.env.PORT || 8080;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Railway server working");
-});
-
-server.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port", PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on", PORT);
 });
